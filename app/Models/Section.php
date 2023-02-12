1b8ad4861c473775c6096bda848f1c965fcd3db6
emmageo3 <kanfanyemma22@gmail.com>
2023-02-12 14:49:00 +0000

    display sections

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -9,6 +9,11 @@ class Section extends Model
 {
     use HasFactory;
 
+    public static function sections(){
+        $getSections = Section::with('categories')->where('status', 1)->get()->toArray();
+        return $getSections;
+    }
+
     public function categories(){
         return $this->hasMany('App\Models\Category','section_id')->where(['parent_id'=>0, 'status'=>1])->with('subcategories');
     }
